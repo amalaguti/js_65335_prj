35ec3b88febf401c99df10328fbf858dff6cc181
amalaguti <adrianma78@hotmail.com>
2024-12-06 09:49:56 -0300

    feat: luxon dt_ functions

--- luxon.js
+++ luxon.js
@@ -1,21 +1,11 @@
-//console.dir(luxon);
-
-
-// const now = luxon.DateTime.now();
-// const nowUTC = now.toUTC();
-// const later = luxon.DateTime.fromISO("2024-12-04T16:42")
-// cslog(now.toString());
-// cslog(nowUTC.toString());
-// cslog(later.toString());
-// cslog((later - now) / 60 /60);
-
-// i = luxon.Interval.fromDateTimes(now, later);
-// cslog(`${Math.round(i.length('minutes'))} minutes have passed since ${now.toString()}`);
-
 function dt_now() {
     return luxon.DateTime.now();
 }
 
 function dt_now_str() {
     return dt_now().toString();
+}
+
+function dt_age(base_dt, current_dt, unit = 'seconds') {
+    return Math.round(current_dt.diff(base_dt, unit).as(unit));
 }
